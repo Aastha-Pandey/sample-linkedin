2fdcd43bf5dc3fe7cea783f43b43432211f1c1ef
Aastha Pandey <aasthap7@gmail.com>
2021-11-17 01:06:29 +0530

    style:sidelist

--- src/components/SideFooter.js
+++ src/components/SideFooter.js
@@ -12,7 +12,7 @@ const list = [
 const SideFooter = () => {
   return (
     <>
-      <div className='flex space-y-2 flex-wrap justify-evenly pb-3 items-center border border-r-0 border-l-0 border-t-0 w-full space-x-4'>
+      <div className='flex space-y-2 flex-wrap justify-evenly pb-3 items-end border border-r-0 border-l-0 border-t-0 w-full space-x-4'>
         {list.map((name) => (
           <button className='justify-self-start text-sm text-gray-500 font-medium text-left  '>
             {name}
